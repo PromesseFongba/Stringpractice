05a5ed59e332d16cfe3e2ab55b2f3b681db8004e
unknown <seuglongp@gmail.com>
2023-06-01 15:04:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//let x=15;
+//console.log(x);
+
+//let a=7;
+//console.log(a);
+
+//let names=["jim", "james", "paul", "tyrone"];
+//console.log(names);
+
+let x=12;
+let a=78;
+
+if(x ===12 && a ===78) {
+   let sum=x+a;
+   console.log(sum);
+}
