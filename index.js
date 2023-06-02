59c6406680edb345c94b5b559cebdeac35422b76
unknown <seuglongp@gmail.com>
2023-06-02 14:53:56 -0400

    second commit/if statement

--- index.js
+++ index.js
@@ -8,10 +8,99 @@
 //let names=["jim", "james", "paul", "tyrone"];
 //console.log(names);
 
-let x=12;
-let a=78;
+//let x=12;
+//let a=78;
 
-if(x ===12 && a ===78) {
-   let sum=x+a;
-   console.log(sum);
+//if(x ===12 && a ===78) {
+  // let sum=x+a;
+   //console.log(sum);
+//}
+
+//if (x=10) {
+   // do this
+  // console.log("true");
+//}
+//else{
+   //do this
+  // console.log("false");
+//}
+
+//let age = 18;
+//if (age >18) {
+   // do this
+  // console.log("access granted");
+//}
+//else{
+   //do this
+  // console.log("access denied");
+//}
+
+//let wood= 50;
+//if (wood < 40) {
+   // we have enough for a treehouse
+  // console.log ("shipping");
+//}
+//else {
+   //to bad so sad
+  // console.log ("denied shipping");
+//}
+
+// let username = 'admin';
+// let password = 'pass614';
+
+// if (username === 'admin' && password === 'pass614') {
+//    console.log ("Access granted!");
+// }
+
+// else { 
+//    console.log ("Access denied!");
+// }
+
+// let wood= 19;
+// if (wood >=40) {
+//   //we have enough for a treehouse
+//   console.log ("shipping");
+// }
+// else if (wood >=20 ) {
+//    //to bad so sad
+//   console.log ("denied shipping");
+// }
+// else {
+//   console.log ("you are S.O.L");
+// }
+
+
+// let num = 'promise';
+
+// if (num > 100)  {
+// console.log ("The number is positive and greater than 100.");
+// }
+// else if (num <=100) {
+//  console.log ("The number is positive but less than or equal to 100.");
+// }
+// else if (num <0) {
+// console.log ("The number is negative.");
+//  }
+//  else {
+//    console.log ("not a number")
+//  }
+
+ let grade = '75';
+
+ if (grade>= 80)  {
+ console.log ("You did a great Job!");
+ }
+ else if (grade >= 70 ) {
+  console.log ("keep Trying.");
+ }
+
+ else if (grade>= 60) {
+   console.log ("Ehhhh.");
+ }
+ else if (grade>= 55) {
+      console.log ("not to good");
 }
+  else {
+    console.log ("Bad Grade But You'll Get Em Next Time")
+  }
+    
